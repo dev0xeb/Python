d6b1db6375d7119d839a1406ccb4eb79758fafc0
dev0xeb <clintonayomide96@gmail.com>
2024-09-18 14:39:30 +0100

    commit

--- pythonclasswork/task28.py
+++ pythonclasswork/task28.py
@@ -7,6 +7,19 @@ def listortuple(number):
     return new_number
 print(listortuple("34,67,55,33,12,98"))
 
+def listoftuple2(number):
+    list_number = number.split(',')
+    number_tuple = ()
+    for num in list_number:
+        number_tuple += (num,)
+    list_number = str(list_number)
+    number_tuple = str(number_tuple)
+    new_number = list_number + number_tuple
+    return new_number
+print(listoftuple2("34,67,55,33,12,98"))
+
+
+
 # number = input("Enter number")
 # print(number)
 # number_list = []
