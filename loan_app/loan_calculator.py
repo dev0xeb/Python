4d2ab92ffd2d8200d95a7c3fd1edd5351d588a48
dev0xeb <clintonayomide96@gmail.com>
2024-10-08 18:19:18 +0100

    commit

---
+++ loan_app/loan_calculator.py
@@ -0,0 +1,47 @@
+class LoanCalculator:
+    def __init__(self):
+        self.borrower = None
+        self.interest_rate:int = 24
+        self.loan_amount = None
+        self.loan_period:int = 1
+
+    def set_borrower(self, borrower:str):
+        self.borrower = borrower
+
+    def set_loan_amount(self, amount:int):
+        if amount > 0:
+            self.loan_amount = amount
+        else:
+            raise ValueError("Invalid Loan amount")
+    def get_loan_amount(self):
+        return self.loan_amount
+
+    def get_loan_period(self):
+        return self.loan_period
+
+    def set_loan_period(self, loan_period:int):
+        if loan_period != 1:
+            raise ValueError("Invalid Loan period")
+        else:
+            self.loan_period = 1
+
+    def get_interest_rate(self):
+        return self.interest_rate
+
+    def set_interest_rate(self, interest_rate:int):
+        if interest_rate != 24:
+            raise ValueError("Invalid Interest Rate")
+        else:
+            self.interest_rate = 24
+
+    def get_monthly_payment(self) -> float:
+        monthly_rate = (self.interest_rate / 100)/ 12
+        month_loan_period = self.loan_period * 12
+        monthly_payment = (self.loan_amount * monthly_rate) / (1-(1 + monthly_rate)**-month_loan_period)
+        return round(monthly_payment, 2)
+
+    def get_total_loan_payment(self) -> float:
+        monthly_payment = self.get_monthly_payment()
+        total_loan_payment = self.loan_period * 12
+        total_loan_payment *= monthly_payment
+        return round(total_loan_payment, 2)
---
+++ loan_app/test_loan_calculator.py
@@ -0,0 +1,59 @@
+import unittest
+from loan_app.loan_calculator import LoanCalculator
+
+class TestLoanCalculator(unittest.TestCase):
+    def setUp(self):
+        self.calculator = LoanCalculator()
+
+    def test_intilization(self):
+        self.assertIsNone(self.calculator.borrower)
+        self.assertEqual(self.calculator.interest_rate, 24)
+        self.assertIsNone(self.calculator.loan_amount)
+        self.assertEqual(self.calculator.loan_period, 1)
+
+    def test_set_borrower(self):
+        self.calculator.set_borrower("Ayoade Clinton")
+        self.assertEqual(self.calculator.borrower, "Ayoade Clinton")
+
+    def test_set_interest_rate(self):
+        self.calculator.set_loan_amount(4000)
+        self.assertEqual(self.calculator.loan_amount, 4000)
+
+    def test_set_loan_period(self):
+        self.assertEqual(self.calculator.loan_period, 1)
+
+    def test_monthly_payment(self):
+        self.calculator.set_borrower("Ayoade Clinton")
+        self.calculator.set_loan_amount(4000)
+        self.assertEqual(self.calculator.get_interest_rate(), 24)
+        self.assertEqual(self.calculator.get_loan_period(), 1)
+        self.assertEqual(self.calculator.get_monthly_payment(), 378.24)
+
+    def test_total_loan_payment(self):
+        self.calculator.set_borrower("Ayoade Clinton")
+        self.calculator.set_loan_amount(10000)
+        self.assertEqual(self.calculator.get_interest_rate(), 24)
+        self.assertEqual(self.calculator.get_loan_period(), 1)
+        self.assertEqual(self.calculator.get_total_loan_payment(), 11347.20)
+
+    def test_invalid_loan_amount(self):
+        self.calculator.set_borrower("Ayoade Clinton")
+        self.assertRaises(ValueError, self.calculator.set_loan_amount, -4000)
+
+    def test_interest_rate_cannot_change(self):
+        self.calculator.set_borrower("Ayoade Clinton")
+        self.calculator.set_loan_amount(4000)
+        self.assertRaises(ValueError, self.calculator.set_interest_rate, 1)
+
+    def test_loan_period_cannot_change(self):
+        self.calculator.set_borrower("Ayoade Clinton")
+        self.calculator.set_loan_amount(4000)
+        self.calculator.get_interest_rate()
+        self.assertRaises(ValueError, self.calculator.set_loan_period, 24)
+
+    def test_for_float_loan_amount(self):
+        self.calculator.set_borrower("Ayoade Clinton")
+        self.calculator.set_loan_amount(4000.50)
+        self.calculator.get_interest_rate()
+        self.calculator.get_loan_period()
+        self.assertEqual(self.calculator.get_loan_amount(), 4000.50)
